8f300f3233a55ce0e0b54e942f353155056399e7
Kevin Malakoff <kmalakoff@gmail.com>
2017-01-25 09:13:38 -0800

    Added retry to rabbitmq

--- images/api/server/services/servicebus.js
+++ images/api/server/services/servicebus.js
@@ -1,1 +1,28 @@
-module.exports = function() { return require('servicebus').bus({url: process.env.RABBITMQ_SERVICE_URL}); }
+const sleep = require('sleep-promise');
+
+async function createServiceBus() {
+  return new Promise((resolve, reject) => {
+    console.log('Creating', process.env.RABBITMQ_SERVICE_URL);
+    servicebus = require('servicebus').bus({url: process.env.RABBITMQ_SERVICE_URL});
+    servicebus.on('ready', (err) => {
+      console.log('Service bus ready:', process.env.RABBITMQ_SERVICE_URL);
+      resolve(servicebus);
+    });
+    servicebus.on('error', (err) => { reject(err); });
+  });
+}
+
+module.exports = async function() {
+  return new Promise(async (resolve, reject) => {
+    while (true) {
+      try {
+        let servicebus = await createServiceBus();
+        if (servicebus) return resolve(servicebus);
+      }
+      catch (err) {
+        console.log(err);
+        sleep(500);
+      }
+    }
+  });
+}
