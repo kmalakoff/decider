5874980bcfcaed3cfa06ad4db99dd61e55b0cfa2
Kevin Malakoff <kmalakoff@gmail.com>
2017-01-25 08:44:37 -0800

    Updated dependencies

--- images/servicebus/server/initialize.js
+++ images/servicebus/server/initialize.js
@@ -1,3 +1,3 @@
 const urlFormat = require('url').format;
 
-process.env.RABBITMQ_SERVICE_URL = process.env.RABBITMQ_SERVICE_URL || urlFormat({protocol: 'amqp:', slashes: true, hostname: process.env.RABBITMQ_SERVICE_HOST, port: process.env.RABBITMQ_SERVICE__PORT});
+process.env.RABBITMQ_SERVICE_URL = process.env.RABBITMQ_SERVICE_URL || urlFormat({protocol: 'amqp:', slashes: true, hostname: process.env.RABBITMQ_SERVICE_HOST, port: process.env.RABBITMQ_SERVICE_PORT});
