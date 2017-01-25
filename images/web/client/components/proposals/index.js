d1081b81c415b50db53bfd4e8e73106aac0241d6
Kevin Malakoff <kmalakoff@gmail.com>
2017-01-25 13:42:15 -0800

    Stubbed UI

--- images/web/client/components/proposals/index.js
+++ images/web/client/components/proposals/index.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
+import {Button, Checkbox, Form, Input, Radio, Select, TextArea} from 'semantic-ui-react';
 
 @observer
 export default class Proposals extends Component {
   static contextTypes = {store: React.PropTypes.object.isRequired}
 
-  render() {
-    const {store} = this.context;
+  onSave = (e) => { e.preventDefault(); }
 
+  render() {
     return (
-      <h1>Proposals</h1>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field control={Input} label='Name' placeholder='Name' />
+        </Form.Group>
+        <Form.Field control={Button} onClick={this.onSave}>Create</Form.Field>
+      </Form>
     );
   }
 };
--- images/web/client/components/users/index.js
+++ images/web/client/components/users/index.js
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
 import {observer} from 'mobx-react';
+import {Button, Checkbox, Form, Input, Radio, Select, TextArea} from 'semantic-ui-react';
 
 @observer
 export default class Users extends Component {
   static contextTypes = {store: React.PropTypes.object.isRequired}
 
-  render() {
-    const {store} = this.context;
+  onSave = (e) => { e.preventDefault(); }
 
+  render() {
     return (
-      <h1>Users</h1>
+      <Form>
+        <Form.Group widths='equal'>
+          <Form.Field control={Input} label='First' placeholder='First' />
+          <Form.Field control={Input} label='Last' placeholder='Last' />
+          <Form.Field control={Input} label='Email' placeholder='your@email.com' />
+        </Form.Group>
+        <Form.Field control={Button} onClick={this.onSave}>Create</Form.Field>
+      </Form>
     );
   }
 };
