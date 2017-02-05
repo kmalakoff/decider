caa89b461a66b5e8e49e2bef9438b2c779689206
Kevin Malakoff <kmalakoff@gmail.com>
2017-02-05 08:50:26 -0800

    Refactor

--- server/index.js
+++ server/index.js
@@ -4,22 +4,39 @@ const express = require('express');
 const http = require('http');
 const requireDirectory = require('require-directory');
 
+const PACKAGES = [
+  '../packages/decider-api/server/query',
+  '../packages/decider-api/server/command',
+  '../packages/decider-reducers/server',
+  '../packages/decider-servicebus/server',
+  '../packages/decider-web/server',
+];
+
+const PACKAGE_LOCATIONS = [
+  'routes',
+  'workers'
+];
+
 async function initialize() {
   const app = express();
+  const server = http.Server(app);
   app.use(require('cors')());
   app.use(require('body-parser').json());
 
-  const server = http.Server(app);
+  // initialize services
   const services = requireDirectory(module, './services');
   const options = {app, server, services};
   for (var key in services) services[key] = await services[key](options);
 
-  Object.values(requireDirectory(module, '../packages/decider-api/server/query/routes')).forEach((m) => m(options));
-  Object.values(requireDirectory(module, '../packages/decider-api/server/command/routes')).forEach((m) => m(options));
-  Object.values(requireDirectory(module, '../packages/decider-workers/server/workers')).forEach((m) => m(options));
-  Object.values(requireDirectory(module, '../packages/decider-servicebus/server/workers')).forEach((m) => m(options));
-  Object.values(requireDirectory(module, '../packages/decider-web/server/routes')).forEach((m) => m(options));
+  // initialize modules
+  PACKAGES.forEach((package) => {
+    PACKAGE_LOCATIONS.forEach((location) => {
+      try { Object.values(requireDirectory(module, path.join(package, location))).forEach((m) => m(options)); }
+      catch (err) { if (err.code !== 'ENOENT') throw err; }
+    });
+  })
 
+  // start server
   const PORT = +process.env.PORT;
   try { await server.listen(PORT); console.log(`Server started on port: ${PORT}`); }
   catch(err) { return console.error(`Server failed to start on port: ${PORT}`, err); }
