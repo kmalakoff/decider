fbd72c161a3201723955c2d55e96730681097220
Kevin Malakoff <kmalakoff@gmail.com>
2017-02-05 10:36:17 -0800

    Refactor

--- server/index.js
+++ server/index.js
@@ -1,18 +1,9 @@
 require('./initialize');
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const http = require('http');
 const requireDirectory = require('require-directory');
-
-const PACKAGES = [
-  '../packages/api-domain-administrating',
-  '../packages/api-domain-registering',
-  '../packages/api-domain-voting',
-  '../packages/api-read',
-  '../packages/servicebus',
-  '../packages/web',
-];
-const PACKAGE_MODULES = [path.join('server', 'routes'), path.join('server', 'workers')];
 
 async function initialize() {
   const app = express();
@@ -26,12 +17,12 @@ async function initialize() {
   for (var key in services) services[key] = await services[key](options);
 
   // initialize modules
-  PACKAGES.forEach((package) => {
-    PACKAGE_MODULES.forEach((mod) => {
-      try { Object.values(requireDirectory(module, path.join(package, mod), {visit: (m) => m(options)})); }
-      catch (err) { if (err.code !== 'ENOENT') throw err; }
-    });
-  })
+  const PACKAGES = fs.readdirSync(path.resolve(path.join(__dirname, '..', 'packages')));
+  const PACKAGE_MODULES = [path.join('server', 'routes'), path.join('server', 'workers')];
+  PACKAGES.forEach((package) => PACKAGE_MODULES.forEach((mod) => {
+    try { Object.values(requireDirectory(module, path.join('..', 'packages', package, mod), {visit: (m) => m(options)})); }
+    catch (err) { if (!~['ENOTDIR', 'ENOENT'].indexOf(err.code)) throw err; }
+  }));
 
   // start server
   const PORT = +process.env.PORT;
